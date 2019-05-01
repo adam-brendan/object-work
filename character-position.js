1c63e2f160771789bb58a0f97acbe00943ad683f
adam-brendan <adamcraveiro@gmail.com>
2019-05-01 17:54:21 +0000

    Created a function that prints the index of each character of a given string

---
+++ character-position.js
@@ -0,0 +1,12 @@
+function charPosition(inputString) {
+  var position = {};
+  for (i = 0; i < inputString.length; i++) {
+    if(!position[inputString[i]]) {
+      position[inputString[i]] = [i];
+    } else {
+      position[inputString[i]].push(i);
+    }
+  }
+  return position;
+}
+console.log(charPosition("Mississippi"));
